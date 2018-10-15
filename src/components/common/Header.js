9c18bd4824f15de4019fd7fc38b48492e0c246f0
debottamr <debottamr@localhost.localdomain>
2018-10-15 06:17:36 -0700

    Added some common components

---
+++ src/components/common/Header.js
@@ -0,0 +1,17 @@
+import React, {PropTypes} from 'react';
+import { Link, IndexLink } from 'react-router';
+import LoadingDots from './LoadingDots';
+
+const Header = ({loading}) => {
+  return (
+    <nav>
+     
+    </nav>
+  );
+};
+
+Header.propTypes = {
+
+};
+
+export default Header;
