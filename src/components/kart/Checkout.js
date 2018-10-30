c4bb02069d3809373a49bf8e33eda4a9d20ebff1
Chandan Pandey <cpandey@localhost.localdomain>
2018-10-30 13:44:42 +0530

    styles folder

--- src/components/kart/Checkout.js
+++ src/components/kart/Checkout.js
@@ -1,8 +1,8 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import AddressForm from './AddressForm';
 
 
-export default class Checkout extends PureComponent {
+export default class Checkout {
   render() {
     return (
       <div>
